503e67d03e3d84bcb4cddec141e168d83bb18a40
Tanguy KAKPO <tang.kakpo@gmail.com>
2025-04-09 10:58:17 +0100

    [ADD]

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+    <Accueil />
+  </StrictMode>,
+)
+
+
+
+
---
+++ src/pages/Accueil.jsx
@@ -0,0 +1,12 @@
+// src/pages/Accueil.jsx
+function Accueil() {
+    return (
+      <div className="container mt-5 text-center">
+        <h1>Bienvenue sur la plateforme de création de livres</h1>
+        <p>Connecte-toi ou crée un compte pour commencer à écrire !</p>
+      </div>
+    );
+  }
+  
+  export default Accueil;
+  
